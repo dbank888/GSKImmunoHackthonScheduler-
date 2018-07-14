cc64a2f31f4a4f4b3141747341c1970fd1d0ca4b
Dipro Chowdhury <achowdh1@mail.sfsu.edu>
2018-07-14 16:24:21 -0700

    navbar added

--- twobirds/src/App.js
+++ twobirds/src/App.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Twobirds</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Welcome Back, Taylor Lee</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">Sign Out</NavLink>
+              </NavItem>
+            </Nav>
+        </Navbar>
       </div>
     );
   }
