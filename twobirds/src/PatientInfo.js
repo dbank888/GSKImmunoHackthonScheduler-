ed4e4fb3f11d43a1403b1b91e7f7fb0174fc2368
Dipro Chowdhury <achowdh1@mail.sfsu.edu>
2018-07-15 02:30:34 -0700

    family tree done

--- twobirds/src/PatientInfo.js
+++ twobirds/src/PatientInfo.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import { Form } from 'reactstrap';
 import FormCompoent from './Form';
+import patient1 from'./patient1.png';
+import patient2 from'./patient2.png';
+import patient3 from'./patient3.png';
+import axios from 'axios';
+
 import {
   Navbar,
   NavbarToggler,
@@ -11,10 +16,34 @@ import {
   NavLink,
   Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle,
-  Button} from 'reactstrap';
+  Button,
+  InputGroup,
+  InputGroupAddon, 
+  InputGroupText, 
+  Input } from 'reactstrap';
 import StickyFooter from 'react-sticky-footer';
 
 class PatientInfo extends Component {
+	constructor(){
+		super();
+		this.state={
+			data: []
+		}
+	}
+	componentDidMount() {
+		console.log("Component Did mount Called");
+		var data = {
+          "CallerName": "Margaret-Ann_De_Luca",
+          "PatientName": "Jane_De_Luca"
+        }
+        var headers = {
+            'Content-Type': 'application/json'
+        }
+        axios.get('http://gskhackathon.azurewebsites.net/api/schedule/recommend', data, headers)
+          .then(res => {
+            console.log("Component Did mount Called");
+         }); 
+     }
 	render(){
 	   return(
 		 <div className="form">
@@ -22,26 +51,124 @@ class PatientInfo extends Component {
           style = {{
           	marginTop: "10px",
           	marginBottom: "10px",
-          	marginLeft: "250px",
-            height: "500px",
+          	marginLeft: "200px",
+            height: "1100px",
             width: 3,
             backgroundColor: '#50E3C2'
            }}
          /> 
-		   <div className="customCards">
-             <Card> 
-             <CardBody>
-             <CardTitle>Card title</CardTitle>
-             <CardSubtitle>Card subtitle</CardSubtitle>
-            </CardBody>
-           </Card>
-           <Card> 
-             <CardBody>
-             <CardTitle>Card title</CardTitle>
-             <CardSubtitle>Card subtitle</CardSubtitle>
-            </CardBody>
-           </Card>
+         <div className="customCard2">
+           <h3 style={{marginTop: "1%", textAlign: "center", color: "#50E3C2", textDecoration: "underline"}}>Records Found</h3>
+		  <div className="customCards">
+             <Card>
+              <CardImg top width="100%" src={patient1} alt="Card image cap" /> 
+             </Card>
+            <Card>
+              <CardBody>
+               <CardTitle>Vaccinations</CardTitle>
+                <InputGroup>
+                 <InputGroupAddon addonType="prepend">
+                 <InputGroupText>
+                 <Input addon type="checkbox" aria-label="Checkbox for following text input" />
+                 </InputGroupText>
+                </InputGroupAddon>
+               <Input placeholder="hpv, influenza" />
+              </InputGroup> 
+               <br />
+              <InputGroup>
+                 <InputGroupAddon addonType="prepend">
+                 <InputGroupText>
+                 <Input addon type="checkbox" aria-label="Checkbox for following text input" />
+                 </InputGroupText>
+                 </InputGroupAddon>
+                 <Input placeholder="pneumococcal infection" />
+              </InputGroup> 
+                <br />
+              <InputGroup>
+                 <InputGroupAddon addonType="prepend">
+                 <InputGroupText>
+                 <Input addon type="checkbox" aria-label="Checkbox for following text input" />
+                 </InputGroupText>
+                 </InputGroupAddon>
+                 <Input placeholder="hepatitis b" />
+              </InputGroup>  
+             </CardBody>
+            </Card>
 		   </div>
+           <h3 style={{marginTop: "2%",marginBottom: "1%", textAlign: "center", color: "#50E3C2", textDecoration: "underline"}}>Family Tree</h3>
+		    <div className="customCards">
+             <Card>
+              <CardImg top width="100%" src={patient2} alt="Card image cap" /> 
+             </Card>
+            <Card>
+              <CardBody>
+               <CardTitle>Vaccinations</CardTitle>
+                <InputGroup>
+                 <InputGroupAddon addonType="prepend">
+                 <InputGroupText>
+                 <Input addon type="checkbox" aria-label="Checkbox for following text input" />
+                 </InputGroupText>
+                </InputGroupAddon>
+               <Input placeholder="hpv, influenza" />
+              </InputGroup> 
+               <br />
+              <InputGroup>
+                 <InputGroupAddon addonType="prepend">
+                 <InputGroupText>
+                 <Input addon type="checkbox" aria-label="Checkbox for following text input" />
+                 </InputGroupText>
+                 </InputGroupAddon>
+                 <Input placeholder="pneumococcal infection" />
+              </InputGroup> 
+                <br />
+              <InputGroup>
+                 <InputGroupAddon addonType="prepend">
+                 <InputGroupText>
+                 <Input addon type="checkbox" aria-label="Checkbox for following text input" />
+                 </InputGroupText>
+                 </InputGroupAddon>
+                 <Input placeholder="hepatitis b" />
+              </InputGroup>  
+             </CardBody>
+            </Card>
+		   </div>
+          <div className="customCards">
+             <Card>
+              <CardImg top width="100%" src={patient3} alt="Card image cap" /> 
+             </Card>
+            <Card>
+              <CardBody>
+               <CardTitle>Vaccinations</CardTitle>
+                <InputGroup>
+                 <InputGroupAddon addonType="prepend">
+                 <InputGroupText>
+                 <Input addon type="checkbox" aria-label="Checkbox for following text input" />
+                 </InputGroupText>
+                </InputGroupAddon>
+               <Input placeholder="hpv, influenza" />
+              </InputGroup> 
+               <br />
+              <InputGroup>
+                 <InputGroupAddon addonType="prepend">
+                 <InputGroupText>
+                 <Input addon type="checkbox" aria-label="Checkbox for following text input" />
+                 </InputGroupText>
+                 </InputGroupAddon>
+                 <Input placeholder="pneumococcal infection" />
+              </InputGroup> 
+                <br />
+              <InputGroup>
+                 <InputGroupAddon addonType="prepend">
+                 <InputGroupText>
+                 <Input addon type="checkbox" aria-label="Checkbox for following text input" />
+                 </InputGroupText>
+                 </InputGroupAddon>
+                 <Input placeholder="hepatitis b" />
+              </InputGroup>  
+             </CardBody>
+            </Card>
+		   </div>
+		  </div>
 		  </div>	
 		);
 	}
