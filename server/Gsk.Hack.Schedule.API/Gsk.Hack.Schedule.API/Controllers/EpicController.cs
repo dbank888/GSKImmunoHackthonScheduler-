3a06e27854361f666362800e9f2f8e09add310de
patrick.alburtus@gmail.com <patrick.alburtus@gmail.com>
2018-09-29 18:26:14 -0400

    moved test auth endpoint to patientinfo endpoint, cleaned up some of auth confirmation redirect page

--- server/Gsk.Hack.Schedule.API/Gsk.Hack.Schedule.API/Controllers/EpicController.cs
+++ server/Gsk.Hack.Schedule.API/Gsk.Hack.Schedule.API/Controllers/EpicController.cs
@@ -43,19 +43,9 @@ namespace Gsk.Hack.Schedule.API.Controllers
         [EnableCors("*", "*", "GET")]
         public IHttpActionResult GetPatientInfo()
         {
-            
-
-            return Ok("{\"resourceType\":\"Patient\",\"birthDate\":\"1985-08-01\",\"active\":true,\"gender\":\"male\",\"deceasedBoolean\":false,\"id\":\"Tbt3KuCY0B5PSrJvCu2j-PlK.aiHsu2xUjUM8bWpetXoB\",\"careProvider\":[{\"display\":\"Physician Family Medicine\",\"reference\":\"https:////open-ic.epic.com//Argonaut//api//FHIR//DSTU2//Practitioner//T3Mz3KLBDVXXgaRoee3EKAAB\"}],\"name\":[{\"use\":\"usual\",\"text\":\"Jason Argonaut\",\"family\":[\"Argonaut\"],\"given\":[\"Jason\"]}],\"identifier\":[{\"use\":\"usual\",\"system\":\"urn:oid:1.2.840.114350.1.13.327.1.7.5.737384.0\",\"value\":\"E3826\"},{\"use\":\"usual\",\"system\":\"urn:oid:1.2.3.4\",\"value\":\"203579\"}],\"address\":[{\"use\":\"home\",\"line\":[\"1979 Milky Way Dr.\"],\"city\":\"Verona\",\"state\":\"WI\",\"postalCode\":\"53593\",\"country\":\"US\"},{\"use\":\"temp\",\"line\":[\"5301 Tokay Blvd\"],\"city\":\"MADISON\",\"state\":\"WI\",\"postalCode\":\"53711\",\"country\":\"US\",\"period\":{\"start\":\"2011-08-04T00:00:00Z\",\"end\":\"2014-08-04T00:00:00Z\"}}],\"telecom\":[{\"system\":\"phone\",\"value\":\"608-271-9000\",\"use\":\"home\"},{\"system\":\"phone\",\"value\":\"608-771-9000\",\"use\":\"work\"},{\"system\":\"phone\",\"value\":\"608-771-9000\",\"use\":\"mobile\"},{\"system\":\"fax\",\"value\":\"608-771-9000\",\"use\":\"home\"},{\"system\":\"phone\",\"value\":\"608-771-9000\",\"use\":\"temp\",\"period\":{\"start\":\"2011-08-04T00:00:00Z\",\"end\":\"2014-08-04T00:00:00Z\"}},{\"system\":\"email\",\"value\":\"open@epic.com\"}],\"maritalStatus\":{\"text\":\"Single\",\"coding\":[{\"system\":\"http:////hl7.org//fhir//ValueSet//marital-status\",\"code\":\"S\",\"display\":\"Never Married\"}]},\"communication\":[{\"preferred\":true,\"language\":{\"text\":\"English\",\"coding\":[{\"system\":\"urn:oid:2.16.840.1.113883.6.99\",\"code\":\"en\",\"display\":\"English\"}]}}],\"extension\":[{\"url\":\"http:////hl7.org//fhir//StructureDefinition//us-core-race\",\"valueCodeableConcept\":{\"text\":\"Asian\",\"coding\":[{\"system\":\"2.16.840.1.113883.5.104\",\"code\":\"2028-9\",\"display\":\"Asian\"}]}},{\"url\":\"http:////hl7.org//fhir//StructureDefinition//us-core-ethnicity\",\"valueCodeableConcept\":{\"text\":\"Not Hispanic or Latino\",\"coding\":[{\"system\":\"2.16.840.1.113883.5.50\",\"code\":\"2186-5\",\"display\":\"Not Hispanic or Latino\"}]}},{\"url\":\"http:////hl7.org//fhir//StructureDefinition//us-core-birth-sex\",\"valueCodeableConcept\":{\"text\":\"Male\",\"coding\":[{\"system\":\"http:////hl7.org//fhir//v3//AdministrativeGender\",\"code\":\"M\",\"display\":\"Male\"}]}}]}");
-        }
-
-        [HttpGet]
-        [Route("oauthtest")]
-        [EnableCors("*", "*", "GET")]
-        public IHttpActionResult TestOauth()
-        {
             try
             {
-                if(string.IsNullOrEmpty(Cache.AuthToken.Instance().GetAccessToken()))
+                if (string.IsNullOrEmpty(Cache.AuthToken.Instance().GetAccessToken()))
                 {
                     if (string.IsNullOrEmpty(Cache.AuthToken.Instance().GetCode()))
                     {
@@ -69,7 +59,7 @@ namespace Gsk.Hack.Schedule.API.Controllers
                         return BadRequest(authResponse.ErrorMessage);
                     }
                 }
-                
+
                 Dictionary<string, string> headers = new Dictionary<string, string>();
                 headers.Add("Authorization", "Bearer " + Cache.AuthToken.Instance().GetAccessToken());
 
@@ -83,14 +73,13 @@ namespace Gsk.Hack.Schedule.API.Controllers
                     headers);
 
                 return Ok(new PatientResposne(patientResponse.Body, immunizationResponse.Body));
-               
+
             }
             catch (Exception ex)
             {
                 return InternalServerError(ex);
             }
         }
-
         private static Aaks.RestclientTests.Model.HttpResponse<EpicOauthResponse> GetAuthResponse()
         {
             StringBuilder sb = new StringBuilder();
